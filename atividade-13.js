1820d13c4ed94a76e2a19cafc861af6702c427f2
Ana Júlia <ana.j.ferreira15@aluno.senai.br>
2025-05-30 11:57:44 -0300

    Filtra os elementos do array

---
+++ atividade-13.js
@@ -0,0 +1,12 @@
+let diasFaltas = [
+    "Carlos – Segunda",
+    "Carlos – Terça",
+    "Carlos – Quarta",
+    "Carlos – Quinta",
+    "Carlos – Sexta",
+    "Carlos – Segunda (semana 2)",
+    "Carlos – Terça (semana 2)",
+    "Carlos – Quarta (semana 2)"
+  ];
+
+console.log(`Relatório da primeira semana: ${diasFaltas.splice(0 , 5)} `);
