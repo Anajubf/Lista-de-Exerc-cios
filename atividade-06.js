1dbccd4b8ab5a22a5e56d8201ad3e42bfa570f9c
Ana Júlia <ana.j.ferreira15@aluno.senai.br>
2025-05-30 11:20:48 -0300

    Remove a primeiroitem no array

---
+++ atividade-06.js
@@ -0,0 +1,13 @@
+let mensagens = [
+    "Propaganda indevida",
+    "Bom dia, posso ajudar?",
+    "Preciso de suporte técnico",
+    "Claro, qual o problema?",
+    "Erro ao acessar conta"
+  ];
+
+let elementoRemovido = mensagens.shift();
+
+console.log(`Depois do shift: ${mensagens}`);
+
+console.log(`Elemento removido:" ${elementoRemovido}`);
