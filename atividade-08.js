d0caafe123597e4389adfda6f99693f4151c10ef
Ana Júlia <ana.j.ferreira15@aluno.senai.br>
2025-05-30 11:26:30 -0300

    Adiciona dois novos elementos no array

---
+++ atividade-08.js
@@ -0,0 +1,12 @@
+let chamada = [
+    "Carlos",
+    "Fernanda",
+    "João",
+    "Mariana",
+    "Lucas",
+    "Camila"
+  ];
+
+  chamada.unshift('Beatriz' , 'Rafaela');
+  console.log(`Depois do unshift: ${chamada}`);
+  console.log(chamada);
