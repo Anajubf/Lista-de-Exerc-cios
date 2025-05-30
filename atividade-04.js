e78b4ecd62b4eccf0ab7f70a50624e564bec1be1
Ana Júlia <ana.j.ferreira15@aluno.senai.br>
2025-05-30 11:15:13 -0300

    Remove a última inscrição da lista no array

---
+++ atividade-04.js
@@ -0,0 +1,8 @@
+let inscritos = [
+    "Pedro", "Larissa", "João", "Amanda", "Lucas", "Mariana"
+  ];
+
+  let inscricaoRemovida  = inscritos.pop();
+
+console.log(`Depois do pop: ${inscritos}`);
+console.log(`A inscrição removida foi: ${inscricaoRemovida}`);
