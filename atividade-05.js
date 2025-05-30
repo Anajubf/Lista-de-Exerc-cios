e2acf8a2ef92279eaf2432744036739feaf414ce
Ana Júlia <ana.j.ferreira15@aluno.senai.br>
2025-05-30 11:18:45 -0300

    Remove a primeiroitem no array

---
+++ atividade-05.js
@@ -0,0 +1,13 @@
+let importados = [
+    "carregador portátil",
+    "cabo HDMI",
+    "leitor biométrico",
+    "controle sem fio",
+    "webcam 4K"
+  ];
+
+let elementoRemovidoda = importados.shift();
+
+console.log(`Depois do shift: ${importados}`);
+
+console.log("Elemento removido:", elementoRemovidoda);
