26ca91df741ca51ac9f175539ffcfbfa4224152a
Ana Júlia <ana.j.ferreira15@aluno.senai.br>
2025-05-30 10:59:35 -0300

    Adiciona novos elementos ao array

---
+++ atividade-01.js
@@ -0,0 +1,12 @@
+let promocoes = [
+    "teclado mecânico",
+    "mouse gamer",
+    "monitor ultrawide",
+    "cadeira ergonômica",
+    "webcam HD",
+    "fone bluetooth",
+    "notebook i5",
+    "hub USB"
+  ];
+  
+  promoções.push (`SSD 1TB`, `mesa digitalizadora`);
