7617389c598cf849ae260df2e072586ba0dfb7f0
Ana Júlia <ana.j.ferreira15@aluno.senai.br>
2025-05-30 11:11:43 -0300

    Remove produto com defeito do array

---
+++ atividade-03.js
@@ -0,0 +1,13 @@
+let producao = [
+    "mousepad",
+    "placa de vídeo",
+    "fonte de alimentação",
+    "HD externo",
+    "notebook gamer",
+    "roteador"
+  ];
+
+  let elementoRemovido  = producao.pop();
+
+console.log(`Depois do pop: ${producao}`);
+console.log(`O elemento removido foi: ${elementoRemovido}`);
