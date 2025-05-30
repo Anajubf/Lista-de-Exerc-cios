41895e2ef4d23fc11ddbfb3360b0b0e41be48e6d
Ana Júlia <ana.j.ferreira15@aluno.senai.br>
2025-05-30 11:32:37 -0300

    Verifica elemento no array

---
+++ atividade-10.js
@@ -0,0 +1,9 @@
+let estoque = [
+    "mesa para PC",
+    "cadeira gamer",
+    "monitor 27 polegadas",
+    "hub USB",
+    "mouse sem fio"
+  ];
+  
+  console.log(`A lista inclui cadeira gamer ${estoque.includes("cadeira gamer")}`);
