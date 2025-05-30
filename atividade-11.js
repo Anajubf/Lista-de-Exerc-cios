067c3e20eb76c5cc09da34702c5b091cd2533478
Ana Júlia <ana.j.ferreira15@aluno.senai.br>
2025-05-30 11:35:25 -0300

    Transforma os elementos do array em um string

---
+++ atividade-11.js
@@ -0,0 +1,10 @@
+let ingredientes = [
+    "farinha de trigo",
+    "fermento",
+    "leite",
+    "ovos",
+    "açúcar",
+    "manteiga"
+  ];
+
+  console.log(`Ingredientes como string: ${ingredientes.join(', ')}`);
